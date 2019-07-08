3c5778c46b231a266820f91d800f96f94a48fc2d
Johan Wassberg <jocar@su.se>
2019-07-08 21:28:19 +0200

    Try to clean up stale files.

--- run.sh
+++ run.sh
@@ -4,6 +4,15 @@
 
 set -e
 
+if [ -f /tmp/TelldusClient ]; then
+    rm /tmp/TelldusClient
+fi
+
+if [ -f /tmp/TelldusEvents ]; then
+    rm /tmp/TelldusEvents
+fi
+
+
 socat TCP-LISTEN:50800,reuseaddr,fork UNIX-CONNECT:/tmp/TelldusClient &
 socat TCP-LISTEN:50801,reuseaddr,fork UNIX-CONNECT:/tmp/TelldusEvents &
 
